4322175cba7e5b38dbf1120f5257a48c17a80b2d
Connor Crawford <cdcraw@icloud.com>
2016-09-23 10:01:39 -0400

    Initial Commit

---
+++ TrailMapper/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TrailMapper
+//
+//  Created by Connor Crawford on 9/23/16.
+//  Copyright © 2016 Connor Crawford. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
